efc29c5a8eca2058414272bab160954fb2caf31f
veerumathapati777 <mail2veermathapati@gmail.com>
2021-01-29 12:49:12 +0530

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Learn GitHub
                 </div>
 
                 <div class="links">
